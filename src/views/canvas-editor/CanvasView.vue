2e909686d24cc8e2933fa80616a7d01aaba42c7c
김재희 <jh.kim4@okestro.com>
2024-02-13 22:19:59 +0900

    DragBox 개발 중

--- src/views/canvas-editor/CanvasView.vue
+++ src/views/canvas-editor/CanvasView.vue
@@ -2,26 +2,36 @@
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { useElementStore } from '@/stores/elements';
 import ElementView from './components/ElementView.vue';
-
-const ZOOM_FACTOR = 0.05;
+import DragBox from './components/DragBox.vue';
 
 const canvas = ref<HTMLDivElement>();
-const container = ref<HTMLDivElement>();
 
+// keyboard 
+const spaceKeydown = ref<boolean>(false);
+
+// mouse
 const startDragX = ref<number>(0);
 const startDragY = ref<number>(0);
-const panX = ref<number>(0);
-const panY = ref<number>(0);
-const conWidth = ref<number>(800);
-const conHeight = ref<number>(600);
+
+// zoom 
+const ZOOM_FACTOR = 0.05;
 const scale = ref<number>(1); // 줌 배율
 
-const spaceKeydown = ref<boolean>(false);
+// .element-container
+const container = ref<HTMLDivElement>();
+const conX = ref<number>(0);
+const conY = ref<number>(0);
+const conWidth = ref<number>(800);
+const conHeight = ref<number>(600);
+
+// drag-box
+const showDragbox = ref<boolean>(false);
+const dragboxWidth = ref<number>(0);
+const dragboxHeight = ref<number>(0);
 
 const { state, setSelectedElement } = useElementStore();
 
 function handleMouseDown(e: MouseEvent) {
-  console.log('mouse event: ', e)
   startDragX.value = e.clientX;
   startDragY.value = e.clientY;
   window.addEventListener('mousemove', handleMouseMove);
@@ -29,17 +39,42 @@ function handleMouseDown(e: MouseEvent) {
 }
 
 function handleMouseMove(e: MouseEvent) {
-  if (canvas.value && spaceKeydown.value) {
-    const deltaX = e.clientX - startDragX.value;
-    const deltaY = e.clientY - startDragY.value;
-    panX.value += deltaX;
-    panY.value += deltaY;
+  const deltaX = e.clientX - startDragX.value;
+  const deltaY = e.clientY - startDragY.value;
+  console.log('delta', deltaX, deltaY);
+  
+  // 패닝
+  if (spaceKeydown.value && !showDragbox.value) {
+    conX.value += deltaX;
+    conY.value += deltaY;
     startDragX.value = e.clientX;
     startDragY.value = e.clientY;
+  }
+
+  // 영역 선택 
+  else {
+    showDragbox.value = true; 
+    dragboxWidth.value = deltaX;
+    dragboxHeight.value = deltaY;
+
+    // // 너비를 끝까지 줄일 경우 방향 변경
+    // if (dragboxWidth.value < 0) {
+    //   startDragX.value += dragboxWidth.value; // x좌표값이 기존 r값 (l + width)이 됨
+    //   dragboxWidth.value *= -1; // 너비를 양수로 변환 
+    // }
+
+    // // 높이를 끝까지 줄일 경우 방향 변경
+    // if (dragboxHeight.value < 0) {
+    //   startDragY.value += dragboxHeight.value; // y좌표값이 기존 b값 (t + height)이 됨
+    //   dragboxHeight.value *= -1; // 높이를 양수로 변환
+    // }
+
+    console.log('dragbox width and height', dragboxWidth.value, dragboxHeight.value)
   }
 }
 
 function handleMouseUp() {
+  showDragbox.value = false;
   window.removeEventListener('mousemove', handleMouseMove);
   window.removeEventListener('mouseup', handleMouseUp);
 }
@@ -69,8 +104,8 @@ function handleWheel(e: WheelEvent) {
   const ds = scale.value - prevScale;
   const dw = conWidth.value * ds;
   const dh = conHeight.value * ds;
-  panX.value -= px * dw;
-  panY.value -= py * dh;
+  conX.value -= px * dw;
+  conY.value -= py * dh;
 }
 
 function zoomIn() {
@@ -126,11 +161,18 @@ onBeforeUnmount(() => {
     @wheel="handleWheel"
     @click="handleCanvasClick"
   >
-    <!-- <DesignElement :zoomLevel="scale" :panX="panX" :panY="panY"/> -->
+    <!-- <DragBox 
+      v-if="showDragbox" 
+      :left="startDragX" 
+      :top="startDragY" 
+      :width="dragboxWidth" 
+      :height="dragboxHeight" 
+      :scale="scale"
+    /> -->
     <div 
       ref="container"
       class="element-container"
-      :style="{ left: panX + 'px', top: panY + 'px', width: `${conWidth * scale}px`, height: `${conHeight * scale}px` }"
+      :style="{ left: conX + 'px', top: conY + 'px', width: `${conWidth * scale}px`, height: `${conHeight * scale}px` }"
     >
       <template v-for="element in state.elements" :key="element.id">
         <ElementView 
---
+++ src/views/canvas-editor/components/DragBox.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps({
+  left: { required: true, type: Number },
+  top: { required: true, type: Number },
+  width: { required: true, type: Number },
+  height: { required: true, type: Number },
+  scale: { required: true, type: Number },
+})
+</script>
+
+<template>
+  <div class="container">
+    <div 
+      class="drag-box" 
+      :style="{ 
+        left: left + 'px', 
+        top: top + 'px', 
+        width: width + 'px', 
+        height: height + 'px' 
+      }" 
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.drag-box {
+  position: absolute;
+  background-color: #4597f74d;
+  border: 1px solid #4597f7;
+}
+</style>
